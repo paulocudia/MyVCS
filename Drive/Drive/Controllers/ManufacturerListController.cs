ed377ecbd4e516ec1bad67e030e73541c80318ad
paulocudia <paulocudia@gmail.com>
2017-01-06 22:38:11 +0800

    Created New Projects

---
+++ Drive/Drive/Controllers/ManufacturerListController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drive.Controllers
+{
+    public class ManufacturerListController : Controller
+    {
+        //
+        // GET: /ManufacturerList/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ Drive/Drive/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Drive.Startup))]
+namespace Drive
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
